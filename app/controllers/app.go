b6c6dae87c777092604d865d66bc6f56d6f02c81
Ryan Armstrong <ryan@cavaliercoder.com>
2014-12-31 22:29:04 +0800

    Implemented partial simple login

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,95 @@
+/*
+ * Alexandria CMDB - Open source config management database
+ * Copyright (C) 2014  Ryan Armstrong <ryan@cavaliercoder.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"errors"
+	"fmt"
+	"github.com/cavaliercoder/alexandria-dashboard/app"
+	"github.com/revel/revel"
+	"net/http"
+	"strings"
+)
 
 type App struct {
-	*revel.Controller
+	Controller
 }
 
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+func (c App) Login() revel.Result {
+	return c.Render()
+}
+
+func (c App) ValidateLogin(username string, password string) revel.Result {
+	// TODO: Antiforgery token
+	var err error
+
+	// Request API Key using username and password
+	body := strings.NewReader(fmt.Sprintf(`{"username":"%s","password":"%s"}`, username, password))
+	res, err := c.ApiRequest("POST", "/apikey", body)
+	c.Check(err)
+
+	// Parse the response
+	switch res.StatusCode {
+	case http.StatusUnauthorized:
+		c.Response.Status = http.StatusUnauthorized
+		c.Flash.Error("The credentials you provided do not appear valid")
+		return c.RenderTemplate("app/Login.html")
+
+	case http.StatusOK:
+		// Parse the apiKey
+		result := make(map[string]string)
+		err = c.Bind(res, &result)
+		c.Check(err)
+
+		apiKey := result["apiKey"]
+		c.Session["apiKey"] = apiKey
+
+		// Now that the apiKey is set, fetch the current user details
+		var user app.User
+		res, err = c.ApiRequest("GET", "/users/current", nil)
+		c.Check(err)
+
+		err = c.Bind(res, &user)
+		c.Check(err)
+		c.Flash.Success("Welcome %s!", user.FirstName)
+
+		// and fetch the tenant
+		var tenant app.Tenant
+		res, err = c.ApiRequest("GET", "/tenants/current", nil)
+		c.Check(err)
+
+		err = c.Bind(res, &tenant)
+		c.Check(err)
+
+		// Great success
+		return c.Redirect(App.Index)
+	default:
+		// TODO: Forward unknown errors
+		return c.RenderError(errors.New("Uh-oh! Basghetti Oooooh!"))
+	}
+}
+
+func (c App) Logout() revel.Result {
+	c.Session["apiKey"] = ""
+
+	return c.Redirect(App.Login)
 }
---
+++ app/controllers/controller.go
@@ -0,0 +1,95 @@
+/*
+ * Alexandria CMDB - Open source config management database
+ * Copyright (C) 2014  Ryan Armstrong <ryan@cavaliercoder.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package controllers
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/revel/revel"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type Controller struct {
+	*revel.Controller
+}
+
+func (c Controller) Check(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func (c Controller) ApiRequest(method string, path string, body io.Reader) (*http.Response, error) {
+	// TODO: Add configurable API url
+	url := fmt.Sprintf("http://localhost:3000/api/v1%s", path)
+	method = strings.ToUpper(method)
+	apiKey := c.Session["apiKey"]
+
+	// Create a HTTP client that does not follow redirects
+	// This allows 'Location' headers to be read
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return errors.New("Never follow redirects")
+		},
+	}
+
+	// Create the request
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add request headers
+	if method == "POST" {
+		req.Header.Add("Content-type", "application/json")
+	}
+
+	if apiKey != "" {
+		req.Header.Add("X-Auth-Token", apiKey)
+	}
+
+	// TODO: Update Dashboard user agent string with version info
+	req.Header.Add("User-Agent", "Alexandria CMDB Dashboard")
+
+	// Submit the request
+	res, err := client.Do(req)
+	return res, err
+}
+
+func (c Controller) Bind(res *http.Response, v interface{}) error {
+	if res.Body == nil {
+		return errors.New("Response body is empty")
+	}
+	defer res.Body.Close()
+
+	if ctype := res.Header.Get("Content-Type"); ctype != "application/json" {
+		return errors.New(fmt.Sprintf("Invalid content type: %s", ctype))
+	}
+
+	err := json.NewDecoder(res.Body).Decode(v)
+
+	if err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
